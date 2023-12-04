767afaef20103108e4946c6207d77320b43c9e78
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2023-12-04 14:07:59 -0800

    Update Day1.rb

--- Day1/Day1.rb
+++ Day1/Day1.rb
@@ -1,7 +1,13 @@
 total = 0
+digits = {"one"=>1, "two"=>2, "three"=>3, "four"=>4, "five"=>5, "six"=>6, "seven"=>7, "eight"=>8, "nine"=>9}
 File.foreach("input.txt") do |line|
-  line = line.gsub(/[^0-9]/, '') #remove all chars except numbers
+  #check for all digits, including digits spelled out with letters
+  line = line.scan(/(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/)
+  #puts line
+  line = line.map{|num| num[0].gsub(/one|two|three|four|five|six|seven|eight|nine/, digits)}
+
   #combine first and last digit to form two-digit number
+  puts Integer(line[0]+(line[line.length-1]))
   total += Integer(line[0]+(line[line.length-1]))
 end
 
