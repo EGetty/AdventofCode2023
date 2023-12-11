5b9ab1ba2918a21480ec6a4e5dcce6cbe1adf8ba
Ethan Getty <35879251+EGetty@users.noreply.github.com>
2023-12-11 13:48:05 -0800

    Completed Day 4 part 2

--- Day4/Day4.rb
+++ Day4/Day4.rb
@@ -1,7 +1,10 @@
 total_points = 0
+total_cards = 0
+win_amounts = [] # Used to store the amount of wins each Card has
 
 # Each game is split into one line
 File.foreach("input.txt") do |line|
+  #total_cards+=1
   score = 0
   # Initial setup removing everything except the tickets numbers
   line.slice!(/\d/)
@@ -16,13 +19,29 @@ File.foreach("input.txt") do |line|
   wins = (winning_nums & my_nums).length
   if wins!=0 then score=1 end # Set score to 1 if there's any wins, so we can enter the scoring loop
   # First point is 1, then doubled for each match after first
-  i=1
-  while i < wins do
-    i+=1
+  plays=1
+  while plays < wins do
+    plays+=1
     score*=2
   end
   # Add the total from each ticket
   total_points += score
+  win_amounts.append(wins)
+end
+
+total_wins = Array.new(win_amounts.length) {|i| i = 1} # Keep track of the amount of tickets including copies
+loc = 0 # Use this to keep track of position in the loop
+win_amounts.each do |win|
+  total_wins[loc].times do
+    index = loc+1 # Wins only add future card copies, so we start one card ahead of our current location
+    win.times do
+      total_wins[index] += 1
+      index+=1
+    end
+  end
+  total_cards += total_wins[loc]
+  loc += 1
 end
 
 puts "Total points: #{total_points}"
+puts "Total cards: #{total_cards}"
